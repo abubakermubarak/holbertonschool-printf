a4fdac08f492781fa037e875a31d6c7a13344b0d
abubakermubarak <abubakermubarak95@gmail.com>
2025-04-08 12:08:23 +0000

    task0

--- printf.c
+++ printf.c
@@ -4,20 +4,25 @@
 #include <string.h>
 int _printf(const char *format, ...)
 {
+	/* init variables */
 	int i;
 	int len;
 	char *s;
 	char c;
 	va_list ap, cp;
-	
+	/* make copy of va_list to loop throught */
 	va_copy(cp,ap);
+	/* start looping in optional args */
 	va_start(ap, format);
 	i = 0;
+	/* itrate throught format  while not empty*/
 	while(format[i])
 	{
+		/* search for % */
 		if (format[i] == '%');
 		{
 			i++;
+			/* serach for specifiers */
 			switch format[i]
 			{
 				case 'c':
@@ -33,6 +38,14 @@ int _printf(const char *format, ...)
 					write(1, s, len);
 					break;
 				}
+			
 			}
 		}
+		/* if % not found  print character */
+		c = va_arg(ap, char);
+		write(1, c, 1);
+		/*  increment i by one to go to next character */
+		i++;
 	}
+	return count;
+
