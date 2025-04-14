deb8a834eeead19e392d8f80e7ad8fdf2a8767a6
abubakermubarak <abubakermubarak95@gmail.com>
2025-04-14 08:36:10 +0000

    removing deuging printf from print_int function

--- numbers.c
+++ numbers.c
@@ -1,11 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+
 extern int n;
 int print_int(int long n)
 {
     char *number = (char *) malloc(32 * sizeof(char)); /* size 4 bytes*/
-    printf("inetrnal buffer made\n");
     int i = 0;
     if (n == 0)
     {
@@ -13,7 +12,6 @@ int print_int(int long n)
     }
     else
     {
-        printf("NUMBER is posative\n");
         if (n < 0)
         {
             add_to_buffer('-');
