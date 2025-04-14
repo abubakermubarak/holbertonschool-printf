7be970ce111fdf387e1469a45e8cd3ce6c8571b1
abubakermubarak <abubakermubarak95@gmail.com>
2025-04-14 08:35:07 +0000

    hot fix

--- numbers.c
+++ numbers.c
@@ -1,19 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 extern int n;
 int print_int(int long n)
 {
     char *number = (char *) malloc(32 * sizeof(char)); /* size 4 bytes*/
+    printf("inetrnal buffer made\n");
     int i = 0;
-    if (n = 0)
+    if (n == 0)
     {
         add_to_buffer('0');
     }
     else
     {
+        printf("NUMBER is posative\n");
         if (n < 0)
         {
             add_to_buffer('-');
+            n = -n;
         }
         while (n) /* n > 0 */
         {
@@ -21,7 +25,6 @@ int print_int(int long n)
             n /= 10;
             i++;
         }
-    
         while(i--) /* decrese i untill i = 0*/
         {
             add_to_buffer(number[i]);
