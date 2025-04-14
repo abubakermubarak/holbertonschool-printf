daa392cbd32f60532585b95c4808fe1f9e6bc338
abubakermubarak <abubakermubarak95@gmail.com>
2025-04-14 08:20:54 +0000

    handle 0 & negative in print_int

--- numbers.c
+++ numbers.c
@@ -5,15 +5,27 @@ int print_int(int long n)
 {
     char *number = (char *) malloc(32 * sizeof(char)); /* size 4 bytes*/
     int i = 0;
-    while (n) /* n > 0 */
+    if (n = 0)
     {
-        number[i] = n % 10 + '0';
-        n /= 10;
-        i++;
+        add_to_buffer('0');
     }
-    while(i--) /* decrese i untill i = 0*/
+    else
     {
-        add_to_buffer(number[i]);
+        if (n < 0)
+        {
+            add_to_buffer('-');
+        }
+        while (n) /* n > 0 */
+        {
+            number[i] = n % 10 + '0';
+            n /= 10;
+            i++;
+        }
+    
+        while(i--) /* decrese i untill i = 0*/
+        {
+            add_to_buffer(number[i]);
+        }
     }
     return (0);
 }
