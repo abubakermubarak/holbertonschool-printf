14ea33613d9725c640178180e533660ce643d646
abubakermubarak <abubakermubarak95@gmail.com>
2025-04-14 09:15:00 +0000

    positve binary only

--- binary.c
+++ binary.c
@@ -2,32 +2,20 @@
 #include <stdlib.h>
 
 extern int n;
-int print_binary(int long n)
+int print_binary(unsigned int long n)
 {
     char *binary = (char *) malloc(32 * sizeof(char));
     int i; 
     i = 0;
-    if (n == 0)
+    while(n)
     {
-        add_to_buffer('0');
+        binary[i] = n % 2 + '0';
+        n  = n / 2;
+        i++;
     }
-    else
+    while(i--)
     {
-        if (n < 0)
-        {
-            add_to_buffer('-');
-            n = -n;
-        }
-        while(n)
-        {
-            binary[i] = n % 2 + '0';
-            n  = n / 2;
-            i++;
-        }
-        while(i--)
-        {
-            add_to_buffer(binary[i]);
-        }
+        add_to_buffer(binary[i]);
     }
     return(0);
 }
